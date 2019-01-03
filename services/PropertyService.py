bf602601c40bc7229277b960ce4f987de3138aec
Alan <ustargab@gmail.com>
2019-01-03 23:34:13 +0100

    properties services

--- services/PropertyService.py
+++ services/PropertyService.py
@@ -1,8 +1,11 @@
+import requests
 import yaml
+from requests import RequestException
 
 from Decorators import Singleton
 
 
+# Property service with internal singleton method
 class PropertyService:
     __instance = None
 
@@ -21,7 +24,7 @@ class PropertyService:
         return self.properties[key]
 
     # load properties from file iterative
-    def load_properties(self, file):
+    def load_properties(self, file="bootstrap.yml"):
         stream = open(file, "r")
         docs = yaml.load_all(stream)
         props = {}
@@ -52,6 +55,7 @@ class PropertyService:
         stream.close()
 
 
+# Property service with singleton decorator
 @Singleton
 class PropertyServiceSingleton:
     __instance = None
@@ -64,8 +68,12 @@ class PropertyServiceSingleton:
     def get_property(self, key):
         return self.properties[key]
 
+    def load(self):
+        self.__load_properties("resources/bootstrap.yml")
+        self.__load_remote_properties()
+
     # load properties from file iterative
-    def load_properties(self, file):
+    def __load_properties(self, file):
         stream = open(file, "r")
         docs = yaml.load_all(stream)
         props = {}
@@ -94,6 +102,19 @@ class PropertyServiceSingleton:
         for k1, v1 in self.properties.items():
             print("key : {0} value: {1}".format(k1, v1))
         stream.close()
+
+    # load remote properties from cloud server configuration
+    def __load_remote_properties(self):
+        try:
+            cloud = requests.get('http://localhost:8888/mysmartercv/develop/master')
+            if cloud.status_code != 200:
+                print("Some error ocurred while we requested configuration to configuration service", cloud.status_code)
+            remote = cloud.json()
+            props = remote['propertySources'][0]['source']
+            for k, v in props.items():
+                self.properties[k] = v
+        except RequestException as er:
+            print(er.args[0])
 
 
 # parser dict recursive
--- simplerest.py
+++ simplerest.py
@@ -2,10 +2,9 @@ from datetime import datetime
 
 from flask import Flask
 from flask_restful import Api
-from pipenv.vendor import requests
-from pipenv.vendor.requests import RequestException
 
 from Decorators import security
+
 from services.EmplyeeResource import EmployeeResource
 from services.PropertyService import PropertyServiceSingleton
 
@@ -17,19 +16,7 @@ app = Flask(__name__)
 api = Api(app)
 api.decorators = [security("enabled")]
 print(datetime.now())
-try:
-    cloud = requests.get('http://localhost:8888/mysmartercv/develop/master')
-    if cloud.status_code != 200:
-        print("Some error ocurred while we requested configuration to configuration service", cloud.status_code)
-        exit(1)
-    remote = cloud.json()
-    print(datetime.now())
-    print(remote['propertySources'][0]['source']['test.app.name'])
-    api.add_resource(EmployeeResource, "/api/v1/employees", )
-
-    PropertyServiceSingleton().load_properties("resources/bootstrap.yml")
-    PropertyServiceSingleton().load_properties("resources/bootstrap.yml")
-
-except RequestException as er:
-    print(er.args[0])
-    exit(1)
+prop_service = PropertyServiceSingleton()
+prop_service.load()
+api.add_resource(EmployeeResource, "/api/v1/employees", )
+print("{} prop {}".format(datetime.now(), prop_service.get_property("darwin.security.mode")))
